1c216f42616c0fe87ccc0c90257c90e2abd9abb9
Alexey Lavrenuke <direvius@gmail.com>
2015-07-31 19:24:40 +0300

    config seems to work

--- bfg/cli.py
+++ bfg/cli.py
@@ -1,9 +1,15 @@
-import config
+import bfg.config
+from .scheduling import create
 
 
 def main():
-    c = config.read()
-    
+    c = bfg.config.read()
+    n = c.get("schedules")
+    lp = {}
+    for k, v in n.items():
+        lp[k] = create(v)
+    print(c.get("bfg.events.scenario"))
+
 
 if __name__ == '__main__':
     main()
--- bfg/config.py
+++ bfg/config.py
@@ -1,31 +1,65 @@
 import pytoml
 
 
-class DotDict(dict):
-    """
-    Inspired by
-    http://stackoverflow.com/questions/3031219/python-recursively-access-dict-via-attributes-as-well-as-index-access
-    """
-    def __init__(self, value=None):
+class Configuration(dict):
+    def __init__(self, value=None, root=None):
+        super().__init__()
         if isinstance(value, dict):
+            self.__root__ = root
             for key in value:
                 self.__setitem__(key, value[key])
         else:
             raise TypeError('expected dict')
 
     def __setitem__(self, key, value):
-        if isinstance(value, dict) and not isinstance(value, DotDict):
-            value = DotDict(value)
-        super(DotDict, self).__setitem__(key, value)
+        if isinstance(value, dict) and not isinstance(
+            value, Configuration
+        ):
+            value = Configuration(value, self.__root__ or self)
+        super().__setitem__(key, value)
+
+    def __setattr__(self, key, value):
+        if key[0] is '_':
+            super().__setattr__(key, value)
+        else:
+            self.__setitem__(key, value)
 
     def __getitem__(self, key):
-        return self.get(key)
+        if key[0] is '_':
+            return super().__getitem__(key)
+        value = self.get(key)
+        if not value:
+            raise ValueError("Key not found: '%s'" % key)
+        if isinstance(value, str) and value[0] is '&':
+            value = self.__root__.get(value[1:].split('.'))
+        return value
 
-    __setattr__ = __setitem__
+    def get(self, key):
+        if isinstance(key, str) and '.' in key:
+            key = key.split('.')
+        if isinstance(key, list):
+            if len(key) > 1:
+                next_node = self.get(key[0])
+                if next_node:
+                    return next_node.__getitem__(key[1:])
+                else:
+                    raise ValueError("Key not found: '%s'" % key[0])
+            else:
+                key = key[0]
+        return super().get(key)
     __getattr__ = __getitem__
+
+
+class ComponentFactory(object):
+    def __init__(self, config):
+        self.config = config
+        self.factories = {}
+        for section, section_config in config.items:
+            self.factories[section] = ComponentFactory.init_factory(
+                section, section_config)
 
 
 def read():
     with open('load.toml', 'rb') as fin:
-        config = DotDict(pytoml.load(fin))
+        config = Configuration(pytoml.load(fin))
     return config
